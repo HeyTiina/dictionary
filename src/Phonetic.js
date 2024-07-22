657faeebe76859f476710aa17d99a7f72abd67af
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-07-22 20:46:47 +0200

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,9 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  const phonetics = props.phonetic.phonetics;
-  const phoneticTexts = phonetics.map((phonetic) => phonetic.text);
-  const phoneticString = phoneticTexts.join(", ");
-
   return (
     <div className="Phonetic">
-      <span className="text">{phoneticString}</span>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
